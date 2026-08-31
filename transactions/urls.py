0c3e94dcd4b49d07276c42304c23d2b1c28d7cad
Biggo02 <o19549991@gmail.com>
2026-08-31 17:10:45 +0200

    fix: expose owner purchase decisions

--- transactions/urls.py
+++ transactions/urls.py
@@ -1,3 +1,4 @@
 from django.urls import path
 from .views import request_purchase,my_transactions
-urlpatterns=[path('annonces/<int:pk>/demander/',request_purchase,name='request_purchase'),path('transactions/',my_transactions,name='transactions')]
+from .views_owner import owner_decision
+urlpatterns=[path('annonces/<int:pk>/demander/',request_purchase,name='request_purchase'),path('transactions/',my_transactions,name='transactions'),path('demandes/<int:pk>/<str:decision>/',owner_decision,name='owner_decision')]
