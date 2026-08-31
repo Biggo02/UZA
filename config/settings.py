6fc5cfa65631915576dd3176730a134ce6d49948
Biggo02 <o19549991@gmail.com>
2026-08-31 17:34:47 +0200

    feat(ai): enable product recognition app and OCR dependency settings

--- config/settings.py
+++ config/settings.py
@@ -7,7 +7,7 @@ load_dotenv(BASE_DIR/'.env')
 SECRET_KEY=os.getenv('SECRET_KEY','dev-uza-secret-change-me')
 DEBUG=os.getenv('DEBUG','True').lower()=='true'
 ALLOWED_HOSTS=[x.strip() for x in os.getenv('ALLOWED_HOSTS','127.0.0.1,localhost').split(',') if x.strip()]
-INSTALLED_APPS=['django.contrib.admin','django.contrib.auth','django.contrib.contenttypes','django.contrib.sessions','django.contrib.messages','django.contrib.staticfiles','core','accounts','marketplace','transactions']
+INSTALLED_APPS=['django.contrib.admin','django.contrib.auth','django.contrib.contenttypes','django.contrib.sessions','django.contrib.messages','django.contrib.staticfiles','core','accounts','marketplace','transactions','product_ai']
 MIDDLEWARE=['django.middleware.security.SecurityMiddleware','django.contrib.sessions.middleware.SessionMiddleware','django.middleware.common.CommonMiddleware','django.middleware.csrf.CsrfViewMiddleware','django.contrib.auth.middleware.AuthenticationMiddleware','django.contrib.messages.middleware.MessageMiddleware','django.middleware.clickjacking.XFrameOptionsMiddleware']
 ROOT_URLCONF='config.urls'
 TEMPLATES=[{'BACKEND':'django.template.backends.django.DjangoTemplates','DIRS':[BASE_DIR/'templates'],'APP_DIRS':True,'OPTIONS':{'context_processors':['django.template.context_processors.debug','django.template.context_processors.request','django.contrib.auth.context_processors.auth','django.contrib.messages.context_processors.messages']}}]
@@ -26,3 +26,4 @@ DEFAULT_AUTO_FIELD='django.db.models.BigAutoField'
 LOGIN_URL='/connexion/'
 LOGIN_REDIRECT_URL='/dashboard/'
 LOGOUT_REDIRECT_URL='/'
+TESSERACT_LANG=os.getenv('TESSERACT_LANG','eng+fra')
