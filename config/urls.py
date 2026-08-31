684026522b9dc73bb9a06f6043e125de9cdffb29
Biggo02 <o19549991@gmail.com>
2026-08-31 17:34:55 +0200

    feat(ai): route product recognition

--- config/urls.py
+++ config/urls.py
@@ -2,5 +2,5 @@ from django.contrib import admin
 from django.urls import include, path
 from django.conf import settings
 from django.conf.urls.static import static
-urlpatterns=[path('admin/',admin.site.urls),path('',include('core.urls')),path('',include('accounts.urls')),path('',include('marketplace.urls')),path('',include('transactions.urls'))]
+urlpatterns=[path('admin/',admin.site.urls),path('',include('core.urls')),path('',include('accounts.urls')),path('',include('marketplace.urls')),path('',include('transactions.urls')),path('reconnaissance/',include('product_ai.urls'))]
 if settings.DEBUG: urlpatterns += static(settings.MEDIA_URL,document_root=settings.MEDIA_ROOT)
